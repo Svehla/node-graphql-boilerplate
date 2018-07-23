90e9a8216238712b83f7dec29595642a8a466a20
Jakub Švehla <svehl.jakub@gmail.com>
2018-07-23 23:43:30 +0200

    auto generate types for graphQl

--- src/gql/models/User/types/GraphQLUserRole.ts
+++ src/gql/models/User/types/GraphQLUserRole.ts
@@ -1,24 +1,5 @@
-import { GraphQLEnumType } from 'graphql'
+
 import { UserRole } from '../../../../constants'
+import getEnumType from '../../../types/getEnumType'
 
-export default new GraphQLEnumType({
-  name: 'GraphQLUserRole',
-  values: {
-    [UserRole.Admin]: {
-      value: UserRole.Admin,
-    },
-    [UserRole.Editor]: {
-      value: UserRole.Editor,
-    },
-    [UserRole.Author]: {
-      value: UserRole.Author,
-    },
-    [UserRole.Contributor]: {
-      value: UserRole.Contributor,
-    },
-    [UserRole.Subscriber]: {
-      value: UserRole.Subscriber,
-    },
-  },
-})
-
+export default getEnumType('GraphQLUserRole', Object.keys(UserRole))
---
+++ src/gql/types/getEnumType.ts
@@ -0,0 +1,16 @@
+import { GraphQLEnumType } from 'graphql'
+
+export default (
+  typeName: string,
+  possibleValues: string[]
+) => (
+   new GraphQLEnumType({
+    name: typeName,
+    values: possibleValues.reduce((pre, name) => {
+      pre[name] = {
+        value: name,
+      }
+      return pre
+    }, {})
+  })
+)
